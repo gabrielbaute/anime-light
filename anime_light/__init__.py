bc7d6e6cc4add13550d387c69bf4f7e62baa98c4
gabrielbaute <gabrielbaute@gmail.com>
2025-12-12 22:31:30 -0400

    Actualización de rutas

--- anime_light/__init__.py
+++ anime_light/__init__.py
@@ -1,4 +1,4 @@
-from anime_light.core.converter import Convert360p, Convert480p, Convert720p, Convert1080p
+from anime_light.core import Convert360p, Convert480p, Convert720p, Convert1080p
 from anime_light.web import create_app
 
 __version__ = "0.3.1"
