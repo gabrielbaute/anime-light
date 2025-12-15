8228c8d787e9c733f241ecb779e334883436076e
gabrielbaute <gabrielbaute@gmail.com>
2025-12-15 11:46:35 -0400

    Entrypoint

---
+++ animelight/__init__.py
@@ -0,0 +1,12 @@
+from animelight.core import Convert360p, Convert480p, Convert720p, Convert1080p
+
+__version__ = "0.3.2"
+
+__all__ = [
+    "Convert360p",
+    "Convert480p",
+    "Convert720p",
+    "Convert1080p",
+    "create_app",
+    "__version__",
+]
