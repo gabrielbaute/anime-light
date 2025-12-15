328b9058cbc75bb115be483cbf5353f463407c7a
gabrielbaute <gabrielbaute@gmail.com>
2025-12-15 11:45:45 -0400

    Parser de comandos para argparse

---
+++ animelight/cli/parser.py
@@ -0,0 +1,57 @@
+import argparse
+
+def create_parser() -> argparse.ArgumentParser:
+    """
+    Commands input configurations
+
+    Returns:
+        argparse.ArgumentParser: Parser object for argeparser cli.
+    """
+    parser = argparse.ArgumentParser(
+        prog="anime-light",
+        
+        usage="Convert video files to lightweight mp4 using ffmpeg.",
+        description="Anime Light Video Converter CLI.",
+        epilog="Example: anime-light input.mkv -r 720p --crf 23 --preset slow --output-dir ./output",
+        add_help=True,
+        allow_abbrev=True,
+        exit_on_error=True,)
+    
+    subparsers = parser.add_subparsers(dest="command", help="Commands available")
+
+    # -------------------------------------------
+    # Subcommand: version
+    # -------------------------------------------
+    subparsers.add_parser("version", help="Shows CLI version")
+
+    # -------------------------------------------
+    # Subcommand: help
+    # -------------------------------------------
+    subparsers.add_parser("help", help="Shows help")
+
+    # -------------------------------------------
+    # Subcommand: analyze
+    # -------------------------------------------
+    analyze_parser = subparsers.add_parser("analyze", help="Analyze a video file")
+    analyze_parser.add_argument("file", type=str, help="Path to the video file to analyze")
+    
+    # -------------------------------------------
+    # Subcommand: sysinfo
+    # -------------------------------------------
+    subparsers.add_parser("sysinfo", help="Shows system information and requirements")
+
+    # -------------------------------------------
+    # Subcommand: convert
+    # -------------------------------------------
+    convert_parser = subparsers.add_parser("convert", help="Convert a video file")
+    convert_parser.add_argument("input", type=str, help="Input file")
+    convert_parser.add_argument("output", type=str, nargs="?", help="Output file (default: ./output/<filename>_converted.mp4)")
+    convert_parser.add_argument("-r", "--resolution", type=int, choices=[360,480,720,1080], help="Video output resolution (360p, 480p, 720p, 1080p)")
+    convert_parser.add_argument("-c", "--crf", type=int, choices=range(18,29), help="CRF (18-28), example: --crf 23")
+    convert_parser.add_argument("-p", "--preset", type=str, choices=["ultrafast","fast","medium","slow"], help="Convert preset for ffmpeg, example: --preset slow")
+    convert_parser.add_argument("-t", "--threads", type=int, help="Threads number to use, example: --threads 4")
+    convert_parser.add_argument("-g", "--use-gpu", action="store_true", help="Use GPU acceleration if its available, example: --use-gpu intel")
+    convert_parser.add_argument("--cool-mode", action="store_true", help="Forzar un único hilo")
+    convert_parser.add_argument("-b", "--batch", "--recursive", action="store_true", help="Procesar subcarpetas o batch")
+
+    return parser
