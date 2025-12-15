6d20ef8da54f315aa8d7490c4a7677fd4f7f5db9
gabrielbaute <gabrielbaute@gmail.com>
2025-12-15 12:05:09 -0400

    bump version 0.3.2 -> 0.4.0

--- animelight/__init__.py
+++ animelight/__init__.py
@@ -1,6 +1,6 @@
 from animelight.core import Convert360p, Convert480p, Convert720p, Convert1080p
 
-__version__ = "0.3.2"
+__version__ = "0.4.0"
 
 __all__ = [
     "Convert360p",
--- animelight/cli/commands/version.py
+++ animelight/cli/commands/version.py
@@ -3,7 +3,7 @@ from rich.table import Table
 
 console = Console()
 
-__version__ = "0.3.2"
+__version__ = "0.4.0"
 
 def show_version() -> None:
     """
