1d9f18bcd26f80b48bb32edf9429e22da7851482
gabrielbaute <gabrielbaute@gmail.com>
2025-12-15 11:43:11 -0400

    Comando para chequear version de la CLI

---
+++ animelight/cli/commands/version.py
@@ -0,0 +1,28 @@
+from rich.console import Console
+from rich.table import Table
+
+console = Console()
+
+__version__ = "0.3.1"
+
+def show_version() -> None:
+    """
+    Shows the CLI version
+    """
+    console = Console()
+    table = Table(
+        title="[bold magenta]anime-light[/bold magenta]",
+        show_header=False,
+        border_style="blue",
+        padding=(0, 2),
+    )
+    table.add_column("Key", style="cyan", justify="right")
+    table.add_column("Value", style="green")
+    
+    table.add_row("[yellow]Build[/yellow]", "[bold]stable[/bold]")
+    table.add_row("Versión", f"[bold]{__version__}[/bold]")
+    table.add_row("Author", "Gabriel Baute")
+    table.add_row("License", "MIT")
+    table.add_row("Repo", "https://github.com/gabrielbaute/anime-light")
+
+    console.print(table)
