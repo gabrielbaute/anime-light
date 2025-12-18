f1e1f37b7a12db8492488a1bc19f31db413ab31d
gabrielbaute <gabrielbaute@gmail.com>
2025-12-18 18:01:15 -0400

    Prefijo para rutas de la api

--- animelight/api/include_routers.py
+++ animelight/api/include_routers.py
@@ -1,9 +1,9 @@
 from fastapi import FastAPI
 from animelight.api.routes import health_router, sys_info_router
 
-def include_routers(app: FastAPI):
+def include_routers(app: FastAPI, prefix: str):
     """
     Include routers for API routes
     """
-    app.include_router(health_router)
-    app.include_router(sys_info_router)
+    app.include_router(health_router, prefix=prefix, tags=["Health"])
+    app.include_router(sys_info_router, prefix=prefix, tags=["System Info"])
