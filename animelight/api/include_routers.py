7a53538fe8a16638dea4adc20191cbf3d8c07f72
gabrielbaute <gabrielbaute@gmail.com>
2025-12-18 18:25:29 -0400

    Inclusion de app factory en el entry point}

--- animelight/api/include_routers.py
+++ animelight/api/include_routers.py
@@ -1,9 +1,16 @@
 from fastapi import FastAPI
 from animelight.api.routes import health_router, sys_info_router
 
-def include_routers(app: FastAPI, prefix: str):
+def include_routers(app: FastAPI, prefix: str) -> None:
     """
     Include routers for API routes
+
+    Args:
+        app (FastAPI): FastAPI application instance
+        prefix (str): Prefix for API routes
+    
+    Returns:
+        None
     """
     app.include_router(health_router, prefix=prefix, tags=["Health"])
     app.include_router(sys_info_router, prefix=prefix, tags=["System Info"])
--- animelight/api/main.py
+++ animelight/api/main.py
@@ -1,12 +1,24 @@
 import uvicorn
-from fastapi import FastAPI
-from animelight.api.include_routers import include_routers
 
-version = "0.4.0"
+from animelight.settings import Settings
+from animelight.api.app import create_app
 
-app = FastAPI(tittle="AnimeLight API", version=version)
+app = create_app()
 
-include_routers(app)
+def run_server(settings: Settings):
+    """
+    Run the FastAPI server.
+    """
+    uvicorn.run(
+        "animelight.api.main:app",
+        host=settings.API_HOST,
+        port=settings.API_PORT,
+        log_level=settings.LOG_LEVEL.lower(),
+        reload=True,
+    )
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    settings = Settings()
+    settings.load_settings()
+    print(settings.API_PORT)
+    run_server(settings)
